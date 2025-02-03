6db679896d2e5d8811e2438758b13692dfe6fd4d
Abiodun Biobaku <biolafrica@gmail.com>
2025-02-03 14:22:29 +0000

    made footer, header and copyright section dynamic

--- script.js
+++ script.js
@@ -6,7 +6,78 @@ const molaCont = document.querySelector(".js_mola_container");
 const workforceCont = document.querySelector(".js_workforce_container");
 const headerCont = document.querySelector(".js-main-header");
 const subHeaderCont = document.querySelector(".js-small-header");
-const footerEl = document.querySelector(".footer_js")
+const footerEl = document.querySelector(".footer_js");
+const headerEl = document.querySelector(".header_js");
+const copyrightEl = document.querySelector(".copyright_js");
+const year = new Date().getFullYear();
+
+
+footerEl.innerHTML = 
+`
+  <h2><b>Keep in Touch</b></h2>
+  <h5>I am currently specializing in <span class="mern">MERN</span> stack.</h5>
+  <h5>Lets Connect and Build Something Great Together.</h5>
+  
+  <div class="footer-btn">
+    <a href="https://drive.google.com/file/d/1MZeLWNZYkjS5uuqxlUCnSP-5tl6Ibqcm/view?usp=sharing" class="btn" target="_blank">
+      <img src="./icons/Document.svg" > Resume
+    </a>
+    <a href="https://www.linkedin.com/in/abiobaku/" class="btn linkedIn" target="_blank"><img src="./icons/linkedIn 1068.png" alt="https://www.linkedin.com/in/abiobaku/">
+      LinkedIn
+    </a>
+    <a href="mailto:biolafrica@gmail.com" class="btn" target="_blank"><img src="./icons/Email.png" alt=""> Email</a>
+
+  </div>
+`;
+
+headerEl.innerHTML = 
+`
+  <div class="main-header js-main-header">
+
+    <a href="/" class="left-header-container">
+      <img src="./icons/Token.png" alt="abiodun logo">
+    </a>
+    
+    <div class="right-header-container">
+      <h4><a href="./index.html">Home</a></h4>
+      <h4><a href="#project">Projects</a></h4>
+      <img class="hamburger js-hamburger" src="./icons/Menu.svg" alt="hamburger icon">
+    </div>
+
+  </div>
+
+  <div class="small-header js-small-header">
+    <div class="small-top-header">
+    
+      <a href="./index.html" class="left-header-container">
+        <img src="./icons/Token.png" alt="abiodun logo">
+      </a>
+    
+      <div class="right-header-container">
+        <img class="close js-close" src="./icons/Close.svg" alt="cancel icon">
+      </div>
+    
+    </div>
+
+    <div class="small-header-nav">
+
+      <div class="small-header-nav-text">
+        <h3><a href="./index.html" class="nav-bold">Home</a></h3>
+        <h3><a href="#project" class="nav-bold">Project</a></h3>
+      </div>
+      
+      <a href="mailto:biolafrica@gmail.com" class="btn">Email</a>
+
+    </div>
+
+  </div>
+
+`;
+
+copyrightEl.innerHTML = 
+`
+  <h6 class="dark-color">Copyright &copy; ${year} Abiodun Biobaku</h6>
+`;
 
 document.querySelectorAll(".js_project_head").forEach((btn)=>{
   btn.addEventListener('click', (e)=>{
@@ -73,7 +144,4 @@ window.addEventListener("resize", ()=>{
   }
   
 })
-
-footerEl.innerHTML = `
-`
 
